0c2cea12532f65fb15a0616b304a9876197dc496
Edward-Code007 <glezmoradev@gmail.com>
2026-06-02 10:25:07 -0400

    GetProductId_Refactor

--- DistributedCache.src/Features/Products/GetProductById.cs
+++ DistributedCache.src/Features/Products/GetProductById.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Metadata.Ecma335;
 using System.Text.Json;
 using DistributedCache.Data;
 using DistributedCache.Endpoints;
@@ -18,27 +19,33 @@ public class GetProductById : IEndpoint
             var settings = cacheSettings.Value;
             var cacheKey = $"{settings.ProductKeyPrefix}:{id}";
 
-            var cachedJson = await cache.GetStringAsync(cacheKey);
-            if (cachedJson is not null)
-            {
-                var cached = JsonSerializer.Deserialize<Product>(cachedJson);
-                return Results.Ok(new { source = "cache", data = cached });
-            }
+            var productCached = await SearchCache(cache, cacheKey);
+            if (productCached is not null) return Results.Ok(new { source = "cache", data = productCached });
 
             var product = await db.Products.AsNoTracking().FirstOrDefaultAsync(p => p.Id == id);
-            if (product is null)
-                return Results.NotFound(new { message = "Product not found", id });
-
-            var json = JsonSerializer.Serialize(product);
-            var options = new DistributedCacheEntryOptions
-            {
-                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(settings.AbsoluteExpirationMinutes),
-                SlidingExpiration = TimeSpan.FromMinutes(settings.SlidingExpirationMinutes)
-            };
-            await cache.SetStringAsync(cacheKey, json, options);
-
+            if (product is null) return Results.NotFound(new { message = "Product not found", id });
+            
+            await SetCache(cache,product,cacheSettings.Value,cacheKey);
             return Results.Ok(new { source = "database", data = product });
         })
         .WithName("GetProduct");
     }
+    public async static Task<Product?> SearchCache(IDistributedCache cache, string id){
+        var cacheString = await cache.GetStringAsync(id);
+        if (cacheString is not null)
+        {
+            return JsonSerializer.Deserialize<Product>(cacheString);
+        }
+        return null;
+    }
+    public static Task SetCache(IDistributedCache cache, Product product, CacheSettings settings, string cacheKey){
+        var json = JsonSerializer.Serialize(product);
+        var options = new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(settings.AbsoluteExpirationMinutes),
+            SlidingExpiration = TimeSpan.FromMinutes(settings.SlidingExpirationMinutes)
+        };
+        return cache.SetStringAsync(cacheKey, json, options);
+    }
+
 }
